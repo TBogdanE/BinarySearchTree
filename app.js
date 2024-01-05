e332885cd8852214135c8a59f21894347d143b94
Toma Bogdan <tbogdane@TBE>
2024-01-05 22:44:20 +0200

    user can add new values in the tree

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class Tree {
 
   sortAndRemoveDuplicates(array) {
     const arr = [...new Set(array)].sort((a, b) => a - b);
-    console.log("array:", arr);
+    //console.log("array:", arr);
     return arr;
   }
 
@@ -37,10 +37,26 @@ class Tree {
       this.buildTree(sorted.slice(0, mid)),
       this.buildTree(sorted.slice(mid + 1))
     );
-    
+
+    return root;
+  }
+
+  insert(value, root = this.root) {
+    if (root === null) {
+      return new Node(value);
+    }
+
+    if (value > root.value) {
+      root.right = this.insert(value, root.right);
+    } else {
+      root.left = this.insert(value, root.left);
+    }
     return root;
   }
 }
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+tree.insert(17);
+tree.insert(18);
+tree.insert(16);
 prettyPrint(tree.root);
